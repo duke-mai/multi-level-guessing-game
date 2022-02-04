74e5449063eaaec83b5a1f91dd6957fa32f23034
Tan Duc Mai <tan.duc.work@gmail.com>
2022-02-04 23:40:35 +1030

    De-capitalised non-constant variables and change function name

--- operations.py
+++ operations.py
@@ -16,45 +16,45 @@ import random
 
 
 # ---------------------------- Function Definitions ---------------------------
-def guessing(GUESS_RANGE, GUESS_LIMIT):
+def guessing(GUESS_RANGE, guess_limit):
     # Set the initial values.
-    RANDOM = random.randint(1, GUESS_RANGE)
-    GUESS = int(input('What is your guess? '))
-    ATTEMPTS_ALLOWED = GUESS_LIMIT
+    ATTEMPTS_ALLOWED = guess_limit
+    random = random.randint(1, GUESS_RANGE)
+    guess = int(input('What is your guess? '))
     done = False
 
     # Validate the inputted guess.
-    GUESS = InputValidation(GUESS, GUESS_RANGE)
+    guess = InputValidation(guess, GUESS_RANGE)
 
     # Now we have a valid guess.
-    while GUESS_LIMIT > 0 and not done:
-        GUESS_LIMIT -= 1                     # Take one guess = lose one chance
-        if GUESS_LIMIT > 0:
-            if GUESS < RANDOM:
-                print(f'It should be higher than {GUESS}.')
-            elif GUESS > RANDOM:
-                print(f'It should be lower than {GUESS}.')
+    while guess_limit > 0 and not done:
+        guess_limit -= 1                     # Take one guess = lose one chance
+        if guess_limit > 0:
+            if guess < random:
+                print(f'It should be higher than {guess}.')
+            elif guess > random:
+                print(f'It should be lower than {guess}.')
             else:
-                ATTEMPTS_TOOK = ATTEMPTS_ALLOWED - GUESS_LIMIT
-                print(f'You nailed it! And it only took you {ATTEMPTS_TOOK} attempts.')
+                attempts_taken = ATTEMPTS_ALLOWED - guess_limit
+                print(f'You nailed it! And it only took you {attempts_taken} attempts.')
                 done = True
-            if GUESS_LIMIT > 0 and not done:
-                print(f'You still have {GUESS_LIMIT} chances left.\n')
-                GUESS = int(input('Try a new guess: '))
+            if guess_limit > 0 and not done:
+                print(f'You still have {guess_limit} chances left.\n')
+                guess = int(input('Try a new guess: '))
                 # Another input validation loop.
-                GUESS = InputValidation(GUESS, GUESS_RANGE)
-        elif GUESS_LIMIT == 0 and not done:                 # Last chance to guess
-            if GUESS == RANDOM:
+                guess = validate_input(guess, GUESS_RANGE)
+        elif guess_limit == 0 and not done:                 # Last chance to guess
+            if guess == random:
                 print(f'You nailed it! However, it took you all the {ATTEMPTS_ALLOWED} attempts.')
             else:
                 print(
                     f'GAME OVER! It took you more than {ATTEMPTS_ALLOWED} attempts. '
-                    f'The correct number is {RANDOM}.'
+                    f'The correct number is {random}.'
                 )
 
 
-def InputValidation(GUESS, GUESS_RANGE):
-    while not 1 <= GUESS <= GUESS_RANGE:
-        print('TRY AGAIN! Your guess is out of range!\n')
-        GUESS = int(input('What is your guess? '))
-    return GUESS
+def validate_input(guess, GUESS_RANGE):
+    while not 1 <= guess <= GUESS_RANGE:
+        print('ERROR! Your guess is out of range!\n')
+        guess = int(input('Try Again. What is your guess? '))
+    return guess
